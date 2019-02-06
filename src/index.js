a907c62a983765aa0a0a5e05e95e138f24425c72
trapajim <mail@tk-dev.de>
2019-02-06 14:38:02 +0900

    implementation of meta scrapper

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import MetadataRules from "./metadata-rules.js";
+
+function extractMetaData(ruleSet, doc) {
+  let maxPriority = 0,
+    maxValue = "";
+
+  ruleSet.rules.forEach((rule, i) => {
+    const [query, handler] = rule;
+    const element = doc.querySelector(query);
+
+    if (!element) return;
+
+    let priority = ruleSet.rules.length - i;
+
+    if (priority > maxPriority) {
+      maxPriority = priority;
+      maxValue = handler(element);
+    }
+  });
+  return maxValue;
+}
+
+function GetMetadata(doc) {
+  const metadata = {};
+  const ruleSets = MetadataRules;
+
+  Object.keys(ruleSets).map(key => {
+    const ruleSet = ruleSets[key];
+
+    metadata[key] = extractMetaData(ruleSet, doc);
+    console.log(metadata);
+  });
+
+  return metadata;
+}
+
+export default GetMetadata;
